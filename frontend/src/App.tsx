306554cd582c78b8e67cd31fe228cf8c611a61ac
pranee-WT <n210278@rguktn.ac.in>
2025-01-28 16:25:03 +0530

    added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,21 @@ import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import Home from './pages/Home';
+<<<<<<< HEAD
 import './index.css'
+=======
+import Host from './pages/Host';
+>>>>>>> 262891e8a9eff7e8d383a7cbaf220756ecd6a76a
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+<<<<<<< HEAD
         <Route path="/home" element={<Home/>} />
+=======
+        <Route path="/host" element={<Host />} />
+        <Route path="/home" element={<Navigate to="/login" replace />} />
+>>>>>>> 262891e8a9eff7e8d383a7cbaf220756ecd6a76a
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
